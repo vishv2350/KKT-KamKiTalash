b463d314bff300cf07dd9d17d0ef617f89e47f96
Rajneesh <rajneesh2350@yahoo.co.in>
2018-09-11 16:40:07 +0530

    11.09.2018

---
+++ CIC/Mobile/cic_mobile_objective.php
@@ -0,0 +1,46 @@
+	<h2>About CIC</h2>
+	<p>With the launch of innovative schemes by MHRD and National Innovation Council, the University felt that innovation programmes be launched which should enhance creative potential of students, have innovative curriculum, are  more project oriented and tailored to the need of society and industry. </p>
+
+	<p>Such  programmes should be enabling for students to relate themselves to the real world problems in all spheres and look for innovative solution for the problems. University should work closely with all stakeholders in this endeavour. </p>
+
+	<h2>Aim</h2>
+
+	<ul>
+
+		<li>To provide a platform for the University and its partners to forge  mutually beneficial linkages to initiate and assist innovation activities and act as a catalyst and facilitator. </li>
+
+		<li>To create an ecosystem that connects  and facilitates various stakeholders on all aspects of the innovation process including training and support</li>
+
+		<li>To promote innovations, channel various incentives that benefit the cluster and  act as an incubating body managing the growth of innovation in the  ecosystem.</li>
+
+	</ul>
+
+	<h2>Broad Objective</h2>
+
+	<ul>
+
+		<li>Connecting research with application for the benefit of society.  </li>
+
+		<li>Support application oriented research to solve real world problems.</li>
+
+		<li>Focus on developing affordable innovations that can benefit a large number of people and at the same time commercially viable and sustainable. </li>
+
+	</ul>
+
+	<h2>Specific Objective</h2>
+
+	<ul>
+
+		<li>Develop innovative degree programmes.</li>
+
+		<li>Select innovative ideas with potential by conducting training/orientation programmes/modules on innovation and researches.</li>
+
+		<li>Develop ideas into innovative applications.</li>
+
+		<li>Facilitate collaborations and partnerships (industry, academia, Institutions) .</li>
+
+		<li>Commercialize innovations and take them to the end users.</li>
+
+		<li>Develop meaningful linkage with various segments of society..</li>
+
+	</ul>
